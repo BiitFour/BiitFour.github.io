56e46ff90e0d28ef8bed6ebf36adfb752d564814
JorgeLBT <jorge_041200@outlook.com>
2025-08-06 21:32:30 -0600

    Update app.js
    Se comento porque no nos sirve se realizan o se cruzan consultas y no se conecta a la base de datos lo dejare Asi para probar, después lo borramos

--- js/app.js
+++ js/app.js
@@ -1,32 +1,33 @@
-// Registro del service worker
-if("serviceWorker" in navigator){
-    window.addEventListener("load", function(){
-        this.navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err));
-    });
-}
+// // Registro del service worker
+// if("serviceWorker" in navigator){
+//     window.addEventListener("load", function(){
+//         this.navigator.serviceWorker
+//         .register("/serviceWorker.js")
+//         .then(res => console.log("service worker registered"))
+//         .catch(err => console.log("service worker not registered", err));
+//     });
+// }
 
- // // Logica de formulario de contacto
- document.addEventListener("DOMContentLoaded", function () {
-   const contactForm = document.getElementById("contact-form");
+//  // // Logica de formulario de contacto
+//  document.addEventListener("DOMContentLoaded", function () {
+//    const contactForm = document.getElementById("contact-form");
 
-   if (contactForm) {
-     contactForm.addEventListener("submit", function (e) {
-      e.preventDefault();
+//    if (contactForm) {
+//      contactForm.addEventListener("submit", function (e) {
+//       e.preventDefault();
 
-      // Mostrar alerta de éxito
-      Swal.fire({
-        title: '¡Mensaje enviado!',
-        text: 'Gracias por contactarnos. Te responderemos pronto.',
-        icon: 'success',
-        confirmButtonColor: '#4CAF50',
-        confirmButtonText: 'Cerrar'
-      });
+//       // Mostrar alerta de éxito
+//       Swal.fire({
+//         title: '¡Mensaje enviado!',
+//         text: 'Gracias por contactarnos. Te responderemos pronto.',
+//         icon: 'success',
+//         confirmButtonColor: '#4CAF50',
+//         confirmButtonText: 'Cerrar'
+//       });
 
-      // Limpiar formulario
-      contactForm.reset();
-    });
-  }
-});
+//       // Limpiar formulario
+//       contactForm.reset();
+//     });
+//   }
+// });
+
